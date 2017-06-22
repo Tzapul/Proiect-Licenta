27b42e33e0bf9886ee5a21059d300d8c373b3f2c
Paun Victor <victorpaun20@yahoo.com>
2017-06-22 16:44:51 +0300

    Added model for table!

---
+++ domain/src/main/java/ro/ucv/ace/model/Tables.java
@@ -0,0 +1,14 @@
+package ro.ucv.ace.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by itsix on 22/06/2017.
+ */
+@Entity
+@Table(name = "Tables")
+public class Tables {
+
+
+}
